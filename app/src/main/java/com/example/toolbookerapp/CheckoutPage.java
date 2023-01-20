e318978510b358c7cb3ad3551d5f929b3b1ae970
100025467 <100025467@mvla.net>
2023-01-20 12:37:13 -0800

    some stuff

--- app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
+++ app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
@@ -1,4 +1,23 @@
 package com.example.toolbookerapp;
 
-public class CheckoutPage {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class CheckoutPage extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinnerTest1=(Spinner)findViewById(R.id.test1);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.drills, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTest1.setAdapter(adapter);
+    }
 }
