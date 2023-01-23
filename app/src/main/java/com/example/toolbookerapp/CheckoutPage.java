6b7c613622fb79447542622911f7a3811e443dbb
100025467 <100025467@mvla.net>
2023-01-23 12:12:40 -0800

    In class 1/23

--- app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
+++ app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
@@ -1,20 +1,16 @@
 package com.example.toolbookerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckoutPage extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.checkout_page);
         Spinner spinnerTest1=(Spinner)findViewById(R.id.test1);
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.drills, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
