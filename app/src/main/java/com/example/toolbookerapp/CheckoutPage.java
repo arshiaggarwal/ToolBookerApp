c296eb09a2bba5b8da7adae6021c2247308eba7a
100025467 <100025467@mvla.net>
2023-01-27 12:38:47 -0800

    g

--- app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
+++ app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
@@ -1,19 +1,33 @@
 package com.example.toolbookerapp;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
-public class CheckoutPage extends AppCompatActivity{
+import com.example.toolbookerapp.databinding.CheckoutPageBinding;
+
+public class CheckoutPage extends Fragment {
+
+    private CheckoutPageBinding binding;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.checkout_page);
-        Spinner spinnerTest1=(Spinner)findViewById(R.id.test1);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.drills, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerTest1.setAdapter(adapter);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = CheckoutPageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                NavHostFragment.findNavController
+            }
+        });
     }
 }
