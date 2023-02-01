b42e58cd544364c05cfbadccb9080705e1db8945
100025467 <100025467@mvla.net>
2023-02-01 11:29:53 -0800

    2/1

--- app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
+++ app/src/main/java/com/example/toolbookerapp/CheckoutPage.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.toolbookerapp.databinding.CheckoutPageBinding;
 
@@ -24,6 +23,5 @@ public class CheckoutPage extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
 
-        binding.button.setOnClickListener(view1 -> NavHostFragment.findNavController);
     }
 }
